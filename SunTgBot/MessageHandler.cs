3a90159327e1c671cdd79d472d3c5430dd97f3d5
alya <ellliotros@gmail.com>
2024-11-13 17:39:33 +0100

    Move HandleGetTodaysInfo to the required class

--- SunTgBot/MessageHandler.cs
+++ SunTgBot/MessageHandler.cs
@@ -56,6 +56,38 @@ namespace SunTgBot
             return (isSolsticeDay, solsticeType, isDaylightIncreasing);
         }
 
+        private async Task HandleGetTodaysInfo(long chatId)
+        {
+            DateTime date = DateTime.Now.Date.ToLocalTime();
+            float latitude = 51.759050f;
+            float longitude = 19.458600f;
+            string tzId = "Europe/Warsaw";
+
+            string weatherDataJson = await weatherApiManager.GetTimeAsync(latitude, longitude, date, tzId);
+
+            if (!string.IsNullOrEmpty(weatherDataJson))
+            {
+                WeatherData? weatherData = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherData>(weatherDataJson);
+
+                if (weatherData != null)
+                {
+                    string sunriseTimeString = weatherData.SunriseTime?.ToString() ?? "N/A";
+                    string sunsetTimeString = weatherData.SunsetTime?.ToString() ?? "N/A";
+                    string dayLengthString = weatherData.DayLength?.ToString() ?? "N/A";
+
+                    await botClient.SendTextMessageAsync(chatId, $"Sunrise time: {sunriseTimeString}" +
+                        $"\nSunset time: {sunsetTimeString}" +
+                        $"\nThe day length: {dayLengthString}");
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(chatId, "Unable to retrieve weather data.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error fetching weather data");
+            }
         }
 
         public async Task ListenForMessagesAsync(CancellationToken cancellationToken)
